bd6b5349ee443c3a936e5b4915cf55e38cf23cc3
刈田修平 <karitashuhei@karitashuuheinoMacBook-puro.local>
2019-06-23 13:53:47 +0900

    better

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,9 @@ module Shoodia
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+　config.assets.initialize_on_precompile = false
   end
 
-config.assets.initialize_on_precompile = false
 
 end
 
