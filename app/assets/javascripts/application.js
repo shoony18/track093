ae8c21296d36d4f3966012b70a4ae4e1e3c6f7d1
刈田修平 <karitashuhei@karitashuuheinoMacBook-puro.local>
2019-06-24 21:17:26 +0900

    better

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery_ujs
 //= require jquery
+//= require jquery_ujs
 //= require turbolinks
-//= require bootstrap-sprockets
 //= require_tree .
+//= require jquery3
+//= require bootstrap-sprockets
 
 
