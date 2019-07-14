a2ffd71496d3f16867fcb5b601f707696b7ab7d0
刈田修平 <karitashuhei@karitashuuheinoMacBook-puro.local>
2019-07-14 18:29:04 +0900

    better

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
 #    :password => "e68$Rc40",
 #    :authentication => :plain,
 #    :enable_starttls_auto => true
-  }
+  
     
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
