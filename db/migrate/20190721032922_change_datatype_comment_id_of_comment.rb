f6611e8aa04c522a714a1a16dc37996a41434993
刈田修平 <karitashuhei@karitashuuheinoMacBook-puro.local>
2019-07-21 23:07:00 +0900

    better

--- db/migrate/20190721032922_change_datatype_comment_id_of_comment.rb
+++ db/migrate/20190721032922_change_datatype_comment_id_of_comment.rb
@@ -1,5 +1,14 @@
 class ChangeDatatypeCommentIdOfComment < ActiveRecord::Migration[5.0]
-  def change
-      change_column :comments, :comment_id, :integer
-  end
+    def up
+        if Rails.env.development? || Rails.env.test?
+            change_column :comments, :comment_id, :integer    
+        else Rails.env.production?
+      # 本番環境はusingオプションを追加
+            change_column :comments, :comment_id, 'integer USING CAST(comment_id AS integer)'
+        end
+    end
+       
+    def down
+        change_column :comments, :comment_id, :string
+    end
 end
--- db/migrate/20190721042405_change_datatype_article_id_of_comments.rb
+++ db/migrate/20190721042405_change_datatype_article_id_of_comments.rb
@@ -1,5 +1,13 @@
 class ChangeDatatypeArticleIdOfComments < ActiveRecord::Migration[5.0]
-  def change
-      change_column :comments, :article_id, :integer
+  def up
+      if Rails.env.development? || Rails.env.test?
+          change_column :comments, :article_id, :integer    
+      else Rails.env.production?
+      # 本番環境はusingオプションを追加
+          change_column :comments, :article_id, 'integer USING CAST(article_id AS integer)'
+      end
+  end
+    def down
+    change_column :comments, :article_id, :string
   end
 end
